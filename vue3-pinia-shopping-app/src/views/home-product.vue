e0c1efa923ca9df07b25384f3aab6111b19f7e3a
thamakorn.k <thamakorn.aom31@gmail.com>
2023-09-20 00:27:57 +0700

    feat(view): #2 mock data on product views

--- vue3-pinia-shopping-app/src/views/home-product.vue
+++ vue3-pinia-shopping-app/src/views/home-product.vue
@@ -2,6 +2,21 @@
     <div>
         <h1> HOME PRODUCT view</h1>
     </div>
+    <div class="row mt-3">
+        <div class="col-md-3 mb-3" v-for="(index) in 20">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top mx-auto" src="https://keychronthailand.com/cdn/shop/files/Everest-Keycap-Set-6_1400x.jpg?v=1689161438" alt="" style="width: 17rem; height: 12rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Keycap Everest</h5>
+                    <p class="card-text">Keychron Everest Keycap Set</p>
+                    <p class="card-text">ราคา $60</p>
+                    <button class="btn btn-primary mx-2">รายละเอียด</button>
+                    <button class="btn btn-success">เพิ่มลงตะกล้า</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
         
 </template>
     

0703dae1f9070420125d90ebf51c2b818c8ebde6
thamakorn.k <thamakorn.aom31@gmail.com>
2023-09-20 01:39:19 +0700

    feat(product-store): #3 update product store to value and use on app and home page product data response from api and computed value products

--- vue3-pinia-shopping-app/src/store/product-store.js
+++ vue3-pinia-shopping-app/src/store/product-store.js
@@ -6,16 +6,24 @@ export const useProductStore = defineStore('useProductStore', () => {
     //สร้างตัวแปรมาเก็บข้อมูลที่ได้จาก api
     const products = ref({})
 
+
+    // ทุกครั้งที่ค่าจาก api มีการเรียกใช้งานมา ตัว computed ก็จะถูกเรียกใช้งาน
+    const productlist = computed(() => products.value)
+
+
+    //ถ้า api ยังโหลดข้อมูลไม่เสร็จ จะให้ขึ้นว่า กำลังโหลดข้อมูล
+    const load_products = computed(() => products.value.length >0)
+
     const getProducts = async () => {
         //call api
         await axios.get(`${import.meta.env.VITE_API_GET_PRODUCTS}`)
         .then((response) => {
-            console.log(response)
+            products.value = response.data
         }).catch((err) => {
             console.log(err)
         })
     }
 
     // return ค่าที่ได้จาก api ใน ตัวแปรที่จะเอาออกไปใช้ข้างนอก
-    return {getProducts}
+    return {getProducts, productlist, load_products}
 })
--- vue3-pinia-shopping-app/src/views/home-product.vue
+++ vue3-pinia-shopping-app/src/views/home-product.vue
@@ -1,29 +1,40 @@
 <template>
-    <div>
-        <h1> HOME PRODUCT view</h1>
-    </div>
-    <div class="row mt-3">
-        <div class="col-md-3 mb-3" v-for="(index) in 20">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top mx-auto" src="https://keychronthailand.com/cdn/shop/files/Everest-Keycap-Set-6_1400x.jpg?v=1689161438" alt="" style="width: 17rem; height: 12rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Keycap Everest</h5>
-                    <p class="card-text">Keychron Everest Keycap Set</p>
-                    <p class="card-text">ราคา $60</p>
-                    <button class="btn btn-primary mx-2">รายละเอียด</button>
-                    <button class="btn btn-success">เพิ่มลงตะกล้า</button>
-                </div>
-            </div>
+  <div class="d-flex justify-content-center" v-if="!product_store.load_products">
+    <h2>กำลังโหลดข้อมูลสินค้า...</h2>
+  </div>
+  <div class="row mt-3" v-else>
+    <div
+      class="col mb-3"
+      v-for="(product, index) in listproduct"
+      :key="index"
+    >
+      <div class="card" style="width: 18rem">
+        <img
+          class="card-img-top mx-auto"
+          :src="product.image"
+          alt=""
+          style="width: 17rem; height: 12rem"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ product.title }}</h5>
+          <p class="card-text">หมวดหมู่ {{ product.category }}</p>
+          <p class="card-text">ราคา {{ product.price }}</p>
+          <button class="btn btn-primary mx-2">รายละเอียด</button>
+          <button class="btn btn-success">เพิ่มลงตะกล้า</button>
         </div>
-
+      </div>
     </div>
-        
+  </div>
 </template>
-    
-<script setup >
-    
+
+<script setup>
+import { computed } from "vue";
+
+//use product store
+import { useProductStore } from "../store/product-store";
+const product_store = useProductStore();
+
+const listproduct = computed(() => product_store.productlist);
 </script>
-    
-<style>
-    
-</style>
+
+<style></style>

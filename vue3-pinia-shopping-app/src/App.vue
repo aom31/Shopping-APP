1b3a94c1e43feb4385e74358ab0084107ab582f0
thamakorn.k <thamakorn.aom31@gmail.com>
2023-09-19 23:43:02 +0700

    feat: #2 add component navbar and use on app page

--- vue3-pinia-shopping-app/src/App.vue
+++ vue3-pinia-shopping-app/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-   
+  <navbar_product_component />
+  <div class="container">
     <router-view></router-view>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import navbar_product_component from "./components/navbar-product.vue";
+</script>
 
 <style></style>
---
+++ vue3-pinia-shopping-app/src/components/navbar-product.vue
@@ -0,0 +1,29 @@
+<template>
+ <nav class="navbar navbar-expand-lg " style="background-color: #a9cce5;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SHOPSI</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">หน้าแรก</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">ตะกล้าสินค้า</a>
+        </li>
+        
+      </ul>
+      <div class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="ค้นหาเลย..." aria-label="Search">
+        <button class="btn btn-light" type="submit">Search</button>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script setup ></script>
+
+<style></style>

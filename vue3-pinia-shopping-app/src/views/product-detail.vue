d7c287071e30287860298b1bb62e640b6090ba37
thamakorn.k <thamakorn.aom31@gmail.com>
2023-09-23 22:41:49 +0700

    feat(view): #3 update product detail view with mockup template

--- vue3-pinia-shopping-app/src/views/product-detail.vue
+++ vue3-pinia-shopping-app/src/views/product-detail.vue
@@ -1,13 +1,33 @@
 <template>
-        <div class="container">
-            <h1>Hello product detail</h1>
+  <div class="row">
+    <div class="col-md-6">
+      <img
+        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+        alt=""
+        class="img-fluid"
+      />
+    </div>
+    <div class="col-md-6">
+      <h1>Fjallraven - Foldsack No. 1 Backpack</h1>
+      <p>
+        "Your perfect pack for everyday use and walks in the forest. Stash your
+        laptop (up to 15 inches) in the padded sleeve, your everyday"
+      </p>
+      <h4>ราคา 1500 บาท</h4>
+
+      <div class="d-flex mt-3">
+        <div class="btn-group me-auto">
+          <button class="btn btn-outline-primary">เพิ่มสินค้า</button>
+          <button class="btn btn-outline-primary">0</button>
+          <button class="btn btn-outline-primary">ลบสินค้า</button>
         </div>
+        <button class="btn btn-primary">เพิ่มสินค้า</button>
+      </div>
+      
+    </div>
+  </div>
 </template>
-    
-<script setup>
-    
-</script>
-    
-<style>
-    
-</style>
+
+<script setup></script>
+
+<style></style>
